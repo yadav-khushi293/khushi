ec5203fe42fb8e04b31af2ab994fda550afc94ac
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-05-08 15:13:01 +0530

    xyz

---
+++ .vscode/Hosting/Hosting/jsopretor.js
@@ -0,0 +1,55 @@
+/*
+addition +
+subtraction -
+multiplication *
+Division /
+modules(Reminder)%
+Exponentional ** (eg = 2**3=8)
+Incrementd ++
+Decrements--
+*/
+//using basic maths
+/*let num1=4;
+let num2=5;
+let sum = num1 + num2;
+console.log(sum);
+
+let multi = num1 * num2;
+console.log(multi);
+
+let Division = num1 / num2;
+console.log(Division);
+
+let subtraction = num1 -num2;
+console.log(subtraction);
+
+let modules = num1 % num2;
+console.log(modules);
+
+num1 ++;
+console.log(num1);
+num2 --;
+console.log(num2);
+
+/*console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
+console.log(num1 ++);
+console.log(num2--);
+console.log(num2++);
+console.log()*/
+
+/* asingnments opertors
+1. = :Assign
+2.+= :Add & Assign
+3.-= :Subtract & Assign
+4.*= : multiply & Assign
+5./= :Division & Assign
+6.%= :Modules & Assign
+*/
+
+let num1 = 4;
+num1 *= 2;
+console.log(num1);
