0022fa1b4141565e3a818321b7d70fa426b1a997
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-05-12 14:32:50 +0530

    xyz

--- .vscode/Hosting/Hosting/jsopretor.js
+++ .vscode/Hosting/Hosting/jsopretor.js
@@ -62,10 +62,10 @@ console.log("checking", num1==num2);
 /*
 1.== ->Data value
 2.=== -> Data types
-*/
+
 
 let num4="2";
-let num5="3";
+let num5=3;
 console.log("checkout", num4===num5);
 
 let flag1=false;
@@ -74,6 +74,46 @@ console.log(flag1!=true);
 /*
 != ->data value
 !== -> data types
-*/
+
 let flag3=false;
 console.log(flag3!==true);
+
+/* tsak
+>
+<
+>=
+<=
+
+*/
+let num7=5;
+let num8=8;
+
+/*console.log(num7<num8);
+
+let num9=6;
+let num10=10;
+
+console.log(num10>num9);
+
+let num11=12;
+let num12=13;
+
+console.log(num11<=num13);
+
+let num14=14;
+let num15=15;
+
+console.log(num14>=num15);
+
+
+/*
+logical opretar
+|| -> OR --> only one condition can true
+&& ->AND --> both conditon are true
+! -> NOT --> its convert true to false and flase to true 
+*/
+console.log(num1==num2-1 && num7<num8)
+console.log(flag1!=true || num1==num2-1 )
+let flag4= true;
+console.log("flag4:", !flag4 );
+
