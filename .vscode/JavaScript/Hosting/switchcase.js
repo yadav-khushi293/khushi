53bdd8d6151dc8f28e25dc0089c0b5f0ea720650
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-05-15 14:42:43 +0530

    switch case exmapl

---
+++ .vscode/JavaScript/Hosting/switchcase.js
@@ -0,0 +1,214 @@
+
+/*
+let key = true;
+switch(key){
+    case 1:2==2;
+    console.log(true);
+    break;
+
+    case2:3 !=2;
+    console.log(true);
+    break;
+
+    default:
+        console.log(flase);
+
+}
+*/
+
+let day=1;
+switch(day){
+    case 1:
+    console.log("Monday");
+    break;
+
+    case 2:
+    console.log("Tuseday");
+    break;
+
+    case 3:
+    console.log("Wensday");
+    break;
+
+    case 4:
+    console.log("thursday");
+     break;
+
+     case 5:
+     console.log("friday");
+     break;
+
+    case 6:
+    console.log("surtday");
+    break;
+    
+    case 7:
+    console.log("Sunday");
+    break;
+
+     default:
+        console.log("Good day");
+}
+
+/* 2nd exmaple*/
+var cal= 1;
+switch(cal){
+
+    case 1:
+    console.log("+");
+    break;
+
+    case 2:
+    console.log("-");
+    break;
+
+    case 3:
+    console.log("*");
+    break;
+
+    case 4:
+    console.log("%");
+    break;
+
+    case 5:
+     console.log("/");
+    break;
+
+    default:
+    console.log("calculater");
+
+}
+
+//3rd exmaple
+
+ var key = 1;
+
+switch(key){
+
+    case 1:
+    console.log("Excellent");
+    break;
+
+    case 2:
+    console.log("very good");
+    break;
+
+    case 3:
+    console.log("Average");
+    break;
+
+    case 4:
+    console.log("good");
+    break;
+
+    default:
+        console.log("try next time");
+
+}
+
+//4th exmaple
+
+let color= "red";
+switch(color){
+    case "red":
+    console.log("Stop");
+    break;
+
+     case "yellow":
+    console.log("Slow Down");
+    break;
+
+     case "green":
+    console.log("Go");
+    break;
+    
+     default:
+       console.log("stop the vahical and waite for signal ")
+
+}
+
+//5th example
+
+var sum =5;
+
+switch(sum){
+
+    case 1:
+    console.log("JANUARY");
+    break;
+
+    case 2:
+    console.log("FEBRUARY");
+    break;
+
+     case 3:
+    console.log("MARCH");
+    break;
+     case 4:
+    console.log("APRIL");
+    break;
+
+     case 5:
+    console.log("MAY");
+    break;
+
+     case 6:
+    console.log("JUNE");
+    break;
+
+     case 7:
+    console.log("JULY");
+    break;
+
+     case 8:
+    console.log("AUGUST");
+    break;
+
+     case 9:
+    console.log("OCTOBER");
+    break;
+
+     case 10:
+    console.log("SEPTEMBER");
+    break;
+
+     case 11:
+    console.log("NOVEMBER");
+    break;
+
+     case 12:
+    console.log("DECEMBER");
+    break;
+
+    default:
+    console.log("month end");
+}
+
+//6th exmaple
+
+let sum_1 ="chrome"
+switch(sum_1){
+
+     case "chrome":
+    console.log("you are using chrome");
+    break;
+
+     case "firefox":
+    console.log("you are using firefox ");
+    break;
+
+     case "edge":
+    console.log("you are using edge ");
+    break;
+    
+
+     case "opera":
+    console.log("you are using opera ");
+    break;
+
+    default:
+        console.log("browser not supported")
+
+    
+
+}
