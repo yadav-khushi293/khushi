9ddb4df5524c967d71f3e342e9440d4d09bcc0c0
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-05-15 15:55:31 +0530

    xyz

--- .vscode/JavaScript/Hosting/loops.js
+++ .vscode/JavaScript/Hosting/loops.js
@@ -10,8 +10,10 @@ for( let i=0; i<arr.length; i++){
 
 const arry =[1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20];
 for(let i=2; i<arry.length;i+=2){
-
     console.log("i:",arry[i]);
-
 }
-
+let n=5;
+let sum=0;
+for(let i=1;i<=n;i++ ){
+    console.log(n);
+}
