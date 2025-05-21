b309b5f5af50267b26767ac31ffed37305f7a62b
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-05-21 21:10:14 +0530

    xyz

---
+++ .vscode/JavaScript/Hosting/function.js
@@ -0,0 +1,56 @@
+//Function
+/*function sum(){
+    console.log(2+4);
+}
+sum()*/
+/*
+function sub(){
+    return 4-2;
+}
+sub();
+
+let ans=sub();
+console.log("ans:",ans);*/
+
+let num1=7;
+let num2=7;
+let num3=7;
+
+function mul(a,b,c){
+      return a*b*c;
+}
+let sum=mul(num1,num2,num3);
+console.log("sum:",sum);
+/*
+function mul(a,b,c){
+      return a*b*c;
+}
+let sum=mul(7,8,9);
+console.log("sum:",sum);
+
+//demo
+
+let foot= "biryani-veg";
+khushi(food);
+
+function khushi(value){
+    hetuk(value);
+}
+
+function hetuk(value){
+    nishad(value);
+}
+function nishad(value){
+    farhan(value);
+}
+
+function farhan(value){
+    console.log(value);
+}
+*/
+//arrow function//
+
+let sumit=()=>{
+      console.log(3+4);
+}
+sumit();
