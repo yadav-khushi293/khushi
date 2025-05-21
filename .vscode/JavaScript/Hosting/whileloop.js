738b52ae6e200c3173d13ea023d64dd6d3f5eb92
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-05-21 15:09:31 +0530

    while loop

---
+++ .vscode/JavaScript/Hosting/whileloop.js
@@ -0,0 +1,41 @@
+/*et num= 9;
+let i = 0;
+let sum = null;
+while(i<num){
+  sum+= i;
+  i++;
+  console.log("sum:", sum);
+}*/
+//console.log("sum:", sum);
+/*
+let i = 1;
+let num = 100;
+while(i<=100){
+    console.log("i:",i);
+    i++;
+}
+*/
+/*
+let i=2;
+let num=100;
+let sum=null;
+while(i<=100){
+    console.log("i:",i);
+     i+=2;
+}*/
+/*
+let N= parseInt(process.argv[2]);
+let i1 =2;
+while(i1<=N){
+    if(i1 % 2 == 0){
+        console.log(i1);
+    }
+}*/
+let N= parseInt(process.argv[2]);
+let i1 =2;
+while(i1<=N){
+    if(i1 % 2 == 0){
+        console.log(i1);
+    }
+}
+
