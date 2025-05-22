bbd499422d2efd7a9ef6cfc212f5f13648bfcacb
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-05-22 13:03:17 +0530

    xyz

--- .vscode/JavaScript/Hosting/calculator.js
+++ .vscode/JavaScript/Hosting/calculator.js
@@ -35,7 +35,7 @@ switch(opr)
      console.log("invalid opretors");
 }
 */
-/* 
+
 let num1 = parseInt(process.argv[2]);
 let num2 = parseInt(process.argv[3]);
 let opr = process.argv[4];
@@ -65,31 +65,4 @@ switch(opr) {
     break;
   default:
     console.log("Invalid operator");
-}*/
-let num1 = parseInt(process.argv[2]);
-let operator = (process.argv[3]);
-let num2 = parseInt(process.argv[4]);
-
-let calculator = (num1, operator,num2) => {
-    switch (operator) {
-        case "+":
-            console.log("Your Answer Is: ", num1 + num2);
-            break;
-        case "-":
-            console.log("Your Answer Is: ", num1 - num2);
-            break;
-        case "*":
-            console.log("Your Answer Is: ", num1 * num2);
-            break;
-        case "/":
-            console.log("Your Answer Is: ", num1 / num2);
-            break;
-        case "%":
-            console.log("Your Answer Is: ", num1 % num2);
-            break;
-        default:
-        console.log("You Have Not Given Operator/Number1/Number2");
-    }
 }
-calculator(num1, operator, num2);
-
