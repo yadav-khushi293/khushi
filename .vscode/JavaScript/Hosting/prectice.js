c99d2b276c1dcf09fcd4b85c15ddb4c8143d2857
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-05-23 18:31:26 +0530

    xyz

---
+++ .vscode/JavaScript/Hosting/prectice.js
@@ -0,0 +1,28 @@
+/*let correct_password = "12456";
+let userinput =parseInt(process.argv[2]);
+ 
+while(userinput!==correct_password){
+    userinput_1 = parseInt(process.argv[3]);
+    console.log("please try anthor password");
+    return;
+}
+*/
+/*
+let userinput = parseInt(process.argv[2]);
+i=1;
+/*
+while(i<=10){
+    console.log(userinput*i);
+    i++
+}
+    
+while(i<=10){
+    console.log(userinput + "*" + i +"=", userinput*i );
+    i++;
+}*/
+let N = parseInt(process.argv[2]);
+ let i=1;
+ while(i<N){
+    console.log(i);
+   i*=2;
+ }
