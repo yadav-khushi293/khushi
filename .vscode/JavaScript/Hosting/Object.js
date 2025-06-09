a6cda5f0094f2e746472ee938e55e607e4aed623
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-06-09 14:33:10 +0530

    xyz

---
+++ .vscode/JavaScript/Hosting/Object.js
@@ -0,0 +1,16 @@
+let car = {
+    brand :'alto',
+    model :'800',
+    years :'1999',
+
+    atomation: {
+   name :'advance alto',
+   model :'1200',
+   years :'1880',
+    },
+    myfunc: function(){
+        console.log('hello');
+    },
+    
+    hobbies:['cod','gow','gta'],
+}
---
+++ .vscode/JavaScript/Hosting/js.md
@@ -0,0 +1,5 @@
+#what is object in js 
+
+-object literal denoted by this -->{};
+- java script is based on object;
+- react mdn filke ob js.
