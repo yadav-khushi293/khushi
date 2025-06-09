0a9c74db6167b12fc625f427391a453ac1399a26
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-06-09 22:22:51 +0530

    xyz

---
+++ .vscode/JavaScript/Hosting/objectexample.js
@@ -0,0 +1,14 @@
+let student = {
+    name:'mohan',
+    s_id:12,
+    email:'mohanyadax--',
+    city:'andheri',
+
+   car :function(){
+       return anjali;
+   }
+    
+}
+console.log(student);
+console.log(student.name);
+console.log(student.car);
