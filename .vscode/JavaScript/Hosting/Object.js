66bf387866c894bf38ce5b8e64760e23a8cf3f12
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-06-09 15:28:50 +0530

    xyz

--- .vscode/JavaScript/Hosting/Object.js
+++ .vscode/JavaScript/Hosting/Object.js
@@ -1,4 +1,4 @@
-let car = {
+/*let car = {
     brand :'alto',
     model :'800',
     years :'1999',
@@ -8,9 +8,50 @@ let car = {
    model :'1200',
    years :'1880',
     },
+
     myfunc: function(){
         console.log('hello');
     },
     
     hobbies:['cod','gow','gta'],
+}*/
+
+
+let parson = {
+    names:'sumit',
+    age:'20',
+    gender:'male',
+}
+console.log("hellow, my name",parson.names);
+ 
+let car={
+    make:' alto',
+    model:'800',
+    years:'1999'
+
+}
+console.log("car",car);
+
+car.name= 'alto-1';
+car.years=2001;
+delete car.model;
+
+ 
+let example={
+    a:1,
+    b:2,
+    c:3,
+}
+
+console.log(Object.keys(example));
+console.log(Object.values(example));
+
+
+ let fruits={
+        apple:10,
+        banana:5,
+        mango:7,
+};
+for(let key in fruits){
+    console.log("fruit names:",key);
 }
