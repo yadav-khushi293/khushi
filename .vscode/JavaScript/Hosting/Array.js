b40b6e616489f9f49ee0c74430134e5c70d127c2
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-06-17 16:36:06 +0530

    cux

---
+++ .vscode/JavaScript/Hosting/Array.js
@@ -0,0 +1,28 @@
+let arr =['mahfooz','frahan','hetuk','khushi']
+
+let arr1=[{name:'mehfooz_khan',age:88},{name:'khushi',age:188}]
+
+let arr2=[false,true,false]
+
+let arr3=[1,2,3,[4,5,6,[7,8,9,10]]]
+
+let arr4 =[
+    [1,2,3]
+     [1,2,3]
+      [1,2,3]
+       [1,2,3]
+]
+
+console.log(aa2);
+
+
+/*for(let=1; i<=Array.length;i++){
+    console.log(i);
+}*/
+
+/*Array.forEach((Element))=>{
+    console.log(Element);
+});*/
+
+Array.pop()//remove the no
+Array.push()//add a number
