e2bb7862257b8c9f341645a88fc655f72b537c12
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-06-23 15:55:19 +0530

    xyz

---
+++ .vscode/JavaScript/Hosting/hightorder_function.js
@@ -0,0 +1,59 @@
+array=[1,2,3,4,5,6,7,6,7,9];
+//#map same as loop but return array
+//#fiter same as loop but return array
+/*let arr = Array.filter((element,index)=>{
+    return element!==7;
+});
+
+//#same as loop but not return array, its show 'undefine'
+
+let arry1 = array.forEach(element => {
+    console.log(element);
+});
+console.log()*/
+
+//#Reducer
+
+/*let answer = Array.reduce((acc,curn)=>acc+curn);
+console.log('->answer:',answer);*/
+
+/*let number=25;
+ /*let ans=Math.pow(number,3);
+ console.log(ans);
+
+  let ans=Math.sqrt(number);
+ console.log(ans);*/
+
+ /*
+ let num =7.6;
+ console.log('num:',Math.ceil(num));
+
+
+ let num2 =8.7;
+ console.log('num2:',Math.floor(num1));
+*/
+
+
+let num23=3.33;
+console.log('num23:',Math.fround(num23));
+
+let num24=6.4;
+console.log('num24:',Math.round(num24));
+
+function randomNumber(x){
+    console.log(Math.round(Math.fround()* x));
+}
+
+randomNumber(num24);
+
+//log value 
+let num77=16;
+let num74=1;
+
+console.log(Math.log(num77)/Math.log())
+
+
+
+
+
+
