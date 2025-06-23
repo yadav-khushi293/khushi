70f0f819047f19b436adf187f81e88596e67ca64
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-06-23 16:51:28 +0530

    xyz

---
+++ .vscode/JavaScript/Hosting/prectice1.js
@@ -0,0 +1,29 @@
+/*let arry1=['MOHAN','SOHAN','ROHAN','RAJU']
+let answer = arry1.map((element,index)=>element);
+console.log('->answer:',answer);*/
+
+/*let string=arry1.map((Element,index)=>Element.length);
+console.log('string:',string);
+
+
+/*
+let number=25;
+let ans=Math.sqrt(number);
+ console.log(ans);*/
+/*
+ let product={
+     brand: 'alto',
+    model:'800',
+    years:1999,
+ }*/
+/*
+let age=[2000,2003,2001,2002,2006,2008]
+
+let age2=age.map((Element,index)=>2025-Element)
+console.log('age2:',age2);
+*/
+
+let number2=[1,2,3,4,5,6,7,8,9,10]
+
+let arr =number2.filter((element,index)=>element%2==0)
+console.log('number2:',number2);
