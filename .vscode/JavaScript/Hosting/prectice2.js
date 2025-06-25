6e9a0aaab2d526974390908bae4ff7da4f322e4d
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-06-25 23:01:43 +0530

    xyz

---
+++ .vscode/JavaScript/Hosting/prectice2.js
@@ -0,0 +1,24 @@
+/*let ice_cream = new Promise((res,rej)=>{
+
+    let got_ice_cream;
+    
+    retTimeout(()=>{
+        got_ice_cream=false;
+        if(got_ice_cream){
+            res('eat ice cream');
+
+        }else {
+          rej('call mom');
+        }
+        },3000);
+});
+
+ice_cream
+.then(()=>{
+    console.log('eat ice cream');
+})
+
+.catch(()=>{
+    console.log('call mommy');
+});*/
+
