078fe90d5aadcbc9b96e475eab2f201965a15918
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-06-27 17:10:33 +0530

    xyz

--- .vscode/JavaScript/Hosting/Apifetch2.js
+++ .vscode/JavaScript/Hosting/Apifetch2.js
@@ -5,7 +5,63 @@ const api=`https://api.thecatapi.com/v1/images/search?limit=22&breed_ids=beng&ap
 const ApiCall = () => {
   fetch(api)
     .then((res) => res.json())
-    .then((res) => console.log(res))
+    .then((res) => appendData(res))
     .catch((err) => console.log(err));
+};
+
+const appendData = (data)=>{
+ 
+    const mainDiv = document.getElementById('info');
+      
+    data.forEach((el) =>{
+         let breeds = el.breeds[0];
+         
+         //card
+         const CardDiv = document.createElement('div');
+         const img =document.createElement('img');
+         const id = document.createElement('p');
+
+         //detailDiv
+
+         const detailDiv = document.createElement('div');
+         const height = document.createElement('p');
+         const width = document.createElement('p');
+
+         //className
+         
+         CardDiv.className='card_div';
+         detailDiv.className = 'detail_div';
+        
+         //data setting
+
+         img.src = el.url;
+         id.innerText='id:${el.id}';
+         height.innerText = 'height:${el.height}';
+         width.innerText='width:${el.width}';
+       
+
+         //breed section
+
+         const name = document.createElement('h3');
+         const description = document.createElement('p');
+         const origin = document.createElement('p');
+         const life_span = document.createElement('p');
+
+          const detailDiv_1 = document.createElement('div');
+
+          name.innerText='name:${breeds.name}';
+         description.innerText='name:${breeds.description}';
+         origin.innerText='origin:${breeds. origin}';
+         life_span.innerText='life_span:$[breed. life_span}';
+
+
+         detailDiv.append(width,height,origin,life_span);
+         detailDiv_1.append(id,name,detailDiv,description);
+         CardDiv.append(img,detailDiv_1);
+         mainDiv.append(CardDiv);
+
+    });
+   
+  
 };
 
