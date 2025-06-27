c0963ffb2aeb603f0f9bfc1944b622af406f31a8
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-06-27 21:42:29 +0530

    xyz

--- .vscode/JavaScript/Hosting/Apifetch2.js
+++ .vscode/JavaScript/Hosting/Apifetch2.js
@@ -3,10 +3,7 @@ const Api_Key ='live_bxAXdnQu3q8aDrjTeIG2rLwQEc4t7cpCb94MhUZ2JlZjObLB2WOJnhdgTbU
 const api=`https://api.thecatapi.com/v1/images/search?limit=22&breed_ids=beng&api_key=${Api_Key}`;
 
 const ApiCall = () => {
-  fetch(api)
-    .then((res) => res.json())
-    .then((res) => appendData(res))
-    .catch((err) => console.log(err));
+  fetch(api).then((res) => res.json()).then((res) => appendData(res)).catch((err) => console.log(err));
 };
 
 const appendData = (data)=>{
