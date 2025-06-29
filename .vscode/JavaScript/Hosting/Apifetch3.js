4a3224d5ad04ead97a5361510ee756c0bb22eb77
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-06-29 16:26:23 +0530

    xyz

--- .vscode/JavaScript/Hosting/Apifetch3.js
+++ .vscode/JavaScript/Hosting/Apifetch3.js
@@ -48,7 +48,7 @@ const appendData = (data) => {
   data.Search.forEach((el) => {
     const CardDiv = document.createElement('div');
     const img = document.createElement('img');
-    const Title = document.createElement('p');
+    const Title = document.createElement('h2');
     const imdbID = document.createElement('p');
     const Type = document.createElement('p');
     const Year = document.createElement('p');
