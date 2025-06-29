6d649e2d86ae7bc9d9e719893827aba025fbb8f0
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-06-29 16:28:39 +0530

    xyz

--- .vscode/JavaScript/Hosting/Apifetch3.js
+++ .vscode/JavaScript/Hosting/Apifetch3.js
@@ -1,37 +1,3 @@
-/*const api='https://www.omdbapi.com/?s=avengers&apikey=3d15e923'
-
-const ApiCall=()=>{
-     fetch(api)
-    .then((res)=>res.json())
-    .then((res)=>appendData(res))
-    .catch((err)=>console.log(err));
-};
-
-const appendData=(data)=>{
-
- const mainDiv=document.getElementById('info');
-
-  data.search.forEach((el) => {
-    
-    const CardDiv = document.createElement('div');
-    const img = document.createElement('img');
-    const Title = document.createElement('p');
-    const imdbID = document.createElement('p');
-    const Type = document.createElement('p');
-    const Year = document.createElement('p');
-
-    img.src = el.Poster;
-    Title.innerText= 'Title:${el.Title}';
-    imdbID.innerText= 'imdbID:${el.imdbID}';
-     Type.innerText='Type:${el. Type}';
-     Year.innerText='Year:${el.Year}';
-
-   CardDiv.append(img,Title,imdbID,Type, Year);
-   mainDiv.append(CardDiv);
-
- });
-};*/
-
 
 const api = 'https://www.omdbapi.com/?s=avengers&apikey=3d15e923';
 
