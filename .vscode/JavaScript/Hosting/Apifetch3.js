d64b7057583fd0e176f5f87c89d0271e961d14d4
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-06-30 15:55:48 +0530

    xyz

--- .vscode/JavaScript/Hosting/Apifetch3.js
+++ .vscode/JavaScript/Hosting/Apifetch3.js
@@ -1,17 +1,49 @@
+const apiKey = `3d15e923`;
 
-const api = 'https://www.omdbapi.com/?s=avengers&apikey=3d15e923';
 
-const ApiCall = () => {
-  fetch(api)
-    .then((res) => res.json())
-    .then((res) => appendData(res))
-    .catch((err) => console.log(err));
+
+const dataLoad = async () => {
+  const api = `https://www.omdbapi.com/?s=avengers&apikey=${apiKey}`;
+  try{
+    const respons = await fetch(api);
+    const data= await respons.json();
+    appendData(data);
+  }catch(error){
+    console.log(error);
+  }
 };
 
+const dataInput = async (event) => {
+  if (event.key === 'Enter') {
+    const searchApi = document.querySelector('#searchInput');
+    console.log('-> searchApi:', searchApi.value.length);
+
+    if (searchApi.value.length === 0) return;
+
+    const api = `https://www.omdbapi.com/?s=${searchApi.value}&apikey=${apiKey}`;
+
+    try {
+      const respons = await fetch(api);
+      const data = await respons.json();
+      console.log('-> data:', data);
+      appendData(data);
+      searchApi.value = '';
+    } catch (error) {
+      console.log('-> error:', error);
+    }
+  }
+  return;
+};
+   
+
 const appendData = (data) => {
+  console.log(data);
+  
   const mainDiv = document.getElementById('info');
+  mainDiv.innerHTML=''
 
   data.Search.forEach((el) => {
+  
     const CardDiv = document.createElement('div');
     const img = document.createElement('img');
     const Title = document.createElement('h2');
@@ -31,5 +63,3 @@ const appendData = (data) => {
     mainDiv.append(CardDiv);
   });
 };
-
-ApiCall();
