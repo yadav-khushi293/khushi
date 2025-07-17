079af930e2999fda1700c422b06eae9226a23c1d
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-07-17 15:44:30 +0530

    xyz

--- .vscode/JavaScript/Hosting/ToDo_list.js
+++ .vscode/JavaScript/Hosting/ToDo_list.js
@@ -1,36 +1,17 @@
-let  arrStorage = JSON.parse(localStorage.getItem('todos')) || [];
+let  dataBase = JSON.parse(localStorage.getItem('formData')) || []
+console.log( dataBase);
 
-const myToodos = ()=>{
-    const todoVal = document.querySelector('#todos').value;
-    alert('please fill the text...');
-    return;
+function handleForm(e){
+    e.preventDefault()
+    let name = document.querySelector('#taskInput').value;
+
+      let personData ={
+        id:Date.now(),
+        todo,
+      }
+      
+       dataBase.push( personData)
+
+       localStorage.setItem("formData",JSON.stringify(dataBase))
+     //renderTodoList()
 }
-//document's {(set)}
-let data = {
-    id:Math.random().toString(36).substring(2,15),
-    todoText:todoVal,
-    isCompleted:false,
-    isEdits:false,
-};
-arrStorage.unshift(data);
-localStorage.setItem('Todos',JSON.stringify(arrStorage));
-appendData();
-alert('your data has been add in to ls...');
-document.querySelector('#todos').value='';
-
-const appendData = ()=>{
-    const dataInfo=document.querySelector('#dataInfo');
-
-    dataInfo.innerHTML='';
-
-    if(arrStorage.length==0){
-        let notify = document.createElement('h1');
-         notify.innerHTML="you dont have data..."
-         dataTnfo.append(notify);
-        }
-
-        arrStorage && arrStorage.forEach(element => {
-            
-        });
-}
-
