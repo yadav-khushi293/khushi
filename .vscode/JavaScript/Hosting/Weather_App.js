4e7482f288f0bcbab739ac467140768b48a9ad2e
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-07-19 15:59:07 +0530

    xyz

---
+++ .vscode/JavaScript/Hosting/Weather_App.js
@@ -0,0 +1,10 @@
+const api_key = `bc3879d0be0368c59db7c196ee0dd4e5`;
+
+const api = `https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=${api_key}`;
+
+const ApiCall = () => {
+  fetch(api)
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
