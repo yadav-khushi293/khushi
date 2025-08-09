1448721aa3a1057e5855ff42babb5f6fc954999b
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-08-09 22:27:32 +0530

    xyz

--- .vscode/JavaScript/Hosting/if_else.js
+++ .vscode/JavaScript/Hosting/if_else.js
@@ -62,5 +62,102 @@
 // else{
 //     console.log("2");
 // }
+//odd number
+// let num=20; 
+
+// if(num%2!==0){
+//   console.log("number odd");
+// }
+// else{
+//   console.log("number not odd");
+// }
+//even number
+
+// let number = 10;
+
+// if(number%2==0){
+//   console.log("number even");
+// }
+// else{
+//   console.log("odd number");
+// }
+
+//positive or nagative or zero
+// let num = -9;
+
+// if(num>0){
+//   console.log("number positive");
+// }
+// if(num<0){
+//   console.log("number is nagtive");
+// }
+// else{
+//   console.log("number is zero");
+// }
+
+//check greater number
+// const num1 = 10;
+// const num2= 20;
+
+// if(num1<num2){
+//   console.log("number is greter");
+// }
+// else{
+//   console.log("number is less");
+// }
 
 
+//print three largest number
+// let num1=10;
+// let num2=20;
+// let num3=30;
+
+// if(num3>num1){
+//   console.log("number is largest")
+// }
+// if(num3>num2){
+//   console.log("number is largest")
+// }
+// if(num2>num1){
+//   console.log("number is largest")
+// }else
+// {
+//   console.log("number is less");
+// }
+
+// const number =70;
+
+// if(number>=90 && number<=100){
+//   console.log("A");
+// }
+// else if(number>=80 && number<=89){
+//   console.log("B");
+// }
+// else if (number>=70 && number<=79){
+//   console.log("C");
+// }
+// else if(number>=60 && number<=69){
+//   console.log("D")
+// }
+// else{
+//   console.log("next time");
+// }
+
+
+// let age = 70;
+
+// if(age>=0 && age<=12){
+//   console.log("child");
+// }
+// else if(age>=13 && age<=19){
+//   console.log("teen");
+// }
+// else if(age>=20 && age<=59){
+//   console.log("aduilt");
+// }
+// else if(age>=60){
+//   console.log("senior")
+// }
+// else{
+//   console.log("xyz");
+// }
