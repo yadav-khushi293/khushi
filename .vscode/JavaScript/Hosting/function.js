766181c1536c1b99d1440966d41107900f0278dc
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-08-10 21:29:24 +0530

    xyz

--- .vscode/JavaScript/Hosting/function.js
+++ .vscode/JavaScript/Hosting/function.js
@@ -1,56 +1,90 @@
-//Function
-/*function sum(){
-    console.log(2+4);
-}
-sum()*/
-/*
-function sub(){
-    return 4-2;
-}
-sub();
+// //Function
+// /*function sum(){
+//     console.log(2+4);
+// }
+// sum()*/
+// /*
+// function sub(){
+//     return 4-2;
+// }
+// sub();
 
-let ans=sub();
-console.log("ans:",ans);*/
+// let ans=sub();
+// console.log("ans:",ans);*/
 
-let num1=7;
-let num2=7;
-let num3=7;
+// let num1=7;
+// let num2=7;
+// let num3=7;
 
-function mul(a,b,c){
-      return a*b*c;
+// function mul(a,b,c){
+//       return a*b*c;
+// }
+// let sum=mul(num1,num2,num3);
+// console.log("sum:",sum);
+// /*
+// function mul(a,b,c){
+//       return a*b*c;
+// }
+// let sum=mul(7,8,9);
+// console.log("sum:",sum);
+
+// //demo
+
+// let foot= "biryani-veg";
+// khushi(food);
+
+// function khushi(value){
+//     hetuk(value);
+// }
+
+// function hetuk(value){
+//     nishad(value);
+// }
+// function nishad(value){
+//     farhan(value);
+// }
+
+// function farhan(value){
+//     console.log(value);
+// }
+// */
+// //arrow function//
+
+// let sumit=()=>{
+//       console.log(3+4);
+// }
+// sumit();
+// let a= 10;
+// let b=20;
+// let c = 30;
+
+// function sum(a,b,c){
+//     return a+b+c;
+// }
+// let result=sum(2,5,8);
+// console.log(result);
+
+// let sum = (a,b)=>(a*b)
+
+// let sum1=sum(2,6);
+// console.log(sum1);
+
+
+// function khushi(){
+//     console.log("hellow");
+// }
+// khushi()
+
+
+function number(a,b,c){
+    return a*b*c;
 }
-let sum=mul(num1,num2,num3);
-console.log("sum:",sum);
-/*
-function mul(a,b,c){
-      return a*b*c;
-}
-let sum=mul(7,8,9);
-console.log("sum:",sum);
 
-//demo
+let numbe1 = number(2,7,9);
+console.log(numbe1 )
 
-let foot= "biryani-veg";
-khushi(food);
 
-function khushi(value){
-    hetuk(value);
+let num2=()=>{
+    console.log("hello")
 }
-
-function hetuk(value){
-    nishad(value);
-}
-function nishad(value){
-    farhan(value);
-}
-
-function farhan(value){
-    console.log(value);
-}
-*/
-//arrow function//
-
-let sumit=()=>{
-      console.log(3+4);
-}
-sumit();
+num2();
