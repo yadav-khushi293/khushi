4f6ef6beab65fae7761e1730ba4a43cebfb63b64
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-08-10 14:24:02 +0530

    xtz

--- .vscode/JavaScript/Hosting/whileloop.js
+++ .vscode/JavaScript/Hosting/whileloop.js
@@ -62,9 +62,27 @@ while(length>=index){
 console.log("reversStr:",reversStr);
 */
 
-let num =0;
-do{
-    console.log(num);
-    num++;
+// let num =0;
+// do{
+//     console.log(num);
+//     num++;
 
-}while(num<=5);
+// }while(num<=5);
+
+// for(i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+// for(i=1; i<=50; i++){
+//     if(i%2!==0){
+//     console.log(i)
+//     }
+// }
+
+let sum = 0;
+
+for(i=0; i<=100; i++){
+    if(i%2===0){
+        sum+=i;
+    }
+}
