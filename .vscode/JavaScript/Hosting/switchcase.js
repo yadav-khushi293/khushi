d7f6701a37b1b08c42cb5aeccf6f21909a16d967
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-08-10 13:07:48 +0530

    cyxt

--- .vscode/JavaScript/Hosting/switchcase.js
+++ .vscode/JavaScript/Hosting/switchcase.js
@@ -16,200 +16,200 @@ switch(key){
 }
 */
 
-let day=1;
-switch(day){
-    case 1:
-    console.log("Monday");
-    break;
+// let day=1;
+// switch(day){
+//     case 1:
+//     console.log("Monday");
+//     break;
 
-    case 2:
-    console.log("Tuseday");
-    break;
+//     case 2:
+//     console.log("Tuseday");
+//     break;
 
-    case 3:
-    console.log("Wensday");
-    break;
+//     case 3:
+//     console.log("Wensday");
+//     break;
 
-    case 4:
-    console.log("thursday");
-     break;
+//     case 4:
+//     console.log("thursday");
+//      break;
 
-     case 5:
-     console.log("friday");
-     break;
+//      case 5:
+//      console.log("friday");
+//      break;
 
-    case 6:
-    console.log("surtday");
-    break;
+//     case 6:
+//     console.log("surtday");
+//     break;
     
-    case 7:
-    console.log("Sunday");
-    break;
+//     case 7:
+//     console.log("Sunday");
+//     break;
 
-     default:
-        console.log("Good day");
-}
+//      default:
+//         console.log("Good day");
+// }
 
-/* 2nd exmaple*/
-var cal= 1;
-switch(cal){
+// /* 2nd exmaple*/
+// var cal= 1;
+// switch(cal){
 
-    case 1:
-    console.log("+");
-    break;
+//     case 1:
+//     console.log("+");
+//     break;
 
-    case 2:
-    console.log("-");
-    break;
+//     case 2:
+//     console.log("-");
+//     break;
 
-    case 3:
-    console.log("*");
-    break;
+//     case 3:
+//     console.log("*");
+//     break;
 
-    case 4:
-    console.log("%");
-    break;
+//     case 4:
+//     console.log("%");
+//     break;
 
-    case 5:
-     console.log("/");
-    break;
+//     case 5:
+//      console.log("/");
+//     break;
 
-    default:
-    console.log("calculater");
+//     default:
+//     console.log("calculater");
 
-}
+// }
 
-//3rd exmaple
+// //3rd exmaple
 
- var key = 1;
+//  var key = 1;
 
-switch(key){
+// switch(key){
 
-    case 1:
-    console.log("Excellent");
-    break;
+//     case 1:
+//     console.log("Excellent");
+//     break;
 
-    case 2:
-    console.log("very good");
-    break;
+//     case 2:
+//     console.log("very good");
+//     break;
 
-    case 3:
-    console.log("Average");
-    break;
+//     case 3:
+//     console.log("Average");
+//     break;
 
-    case 4:
-    console.log("good");
-    break;
+//     case 4:
+//     console.log("good");
+//     break;
 
-    default:
-        console.log("try next time");
+//     default:
+//         console.log("try next time");
 
-}
+// }
 
-//4th exmaple
+// //4th exmaple
 
-let color= "red";
-switch(color){
-    case "red":
-    console.log("Stop");
-    break;
+// let color= "red";
+// switch(color){
+//     case "red":
+//     console.log("Stop");
+//     break;
 
-     case "yellow":
-    console.log("Slow Down");
-    break;
+//      case "yellow":
+//     console.log("Slow Down");
+//     break;
 
-     case "green":
-    console.log("Go");
-    break;
+//      case "green":
+//     console.log("Go");
+//     break;
     
-     default:
-       console.log("stop the vahical and waite for signal ")
+//      default:
+//        console.log("stop the vahical and waite for signal ")
 
-}
+// }
 
-//5th example
+// //5th example
 
-var sum =5;
+// var sum =5;
 
-switch(sum){
+// switch(sum){
 
-    case 1:
-    console.log("JANUARY");
-    break;
+//     case 1:
+//     console.log("JANUARY");
+//     break;
 
-    case 2:
-    console.log("FEBRUARY");
-    break;
+//     case 2:
+//     console.log("FEBRUARY");
+//     break;
 
-     case 3:
-    console.log("MARCH");
-    break;
-     case 4:
-    console.log("APRIL");
-    break;
+//      case 3:
+//     console.log("MARCH");
+//     break;
+//      case 4:
+//     console.log("APRIL");
+//     break;
 
-     case 5:
-    console.log("MAY");
-    break;
+//      case 5:
+//     console.log("MAY");
+//     break;
 
-     case 6:
-    console.log("JUNE");
-    break;
+//      case 6:
+//     console.log("JUNE");
+//     break;
 
-     case 7:
-    console.log("JULY");
-    break;
+//      case 7:
+//     console.log("JULY");
+//     break;
 
-     case 8:
-    console.log("AUGUST");
-    break;
+//      case 8:
+//     console.log("AUGUST");
+//     break;
 
-     case 9:
-    console.log("OCTOBER");
-    break;
+//      case 9:
+//     console.log("OCTOBER");
+//     break;
 
-     case 10:
-    console.log("SEPTEMBER");
-    break;
+//      case 10:
+//     console.log("SEPTEMBER");
+//     break;
 
-     case 11:
-    console.log("NOVEMBER");
-    break;
+//      case 11:
+//     console.log("NOVEMBER");
+//     break;
 
-     case 12:
-    console.log("DECEMBER");
-    break;
+//      case 12:
+//     console.log("DECEMBER");
+//     break;
 
-    default:
-    console.log("month end");
-}
+//     default:
+//     console.log("month end");
+// }
 
-//6th exmaple
+// //6th exmaple
 
-let sum_1 ="chrome"
-switch(sum_1){
+// let sum_1 ="chrome"
+// switch(sum_1){
 
-     case "chrome":
-    console.log("you are using chrome");
-    break;
+//      case "chrome":
+//     console.log("you are using chrome");
+//     break;
 
-     case "firefox":
-    console.log("you are using firefox ");
-    break;
+//      case "firefox":
+//     console.log("you are using firefox ");
+//     break;
 
-     case "edge":
-    console.log("you are using edge ");
-    break;
+//      case "edge":
+//     console.log("you are using edge ");
+//     break;
     
 
-     case "opera":
-    console.log("you are using opera ");
-    break;
+//      case "opera":
+//     console.log("you are using opera ");
+//     break;
 
-    default:
-        console.log("browser not supported")
+//     default:
+//         console.log("browser not supported")
 
-}
+// }
 
 
-console.log("your name ")
+// console.log("your name ")
