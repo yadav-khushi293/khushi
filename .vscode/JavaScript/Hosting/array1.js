b9ee623f540644148ef56dd887c14dffb50adb67
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-08-15 20:47:40 +0530

    xyz

--- .vscode/JavaScript/Hosting/array1.js
+++ .vscode/JavaScript/Hosting/array1.js
@@ -1,3 +1,8 @@
-const khshi=[1,2,3,4,5,6,7];
-khshi.slice(1,5);
-console.log(khshi);
+// const khshi=[1,2,3,4,5,6,7];//splice=(startindex,remover index);
+// let number=khshi.slice(1,5);
+// console.log(khshi);
+
+
+let num1=[1,2,3,5,6,8];
+let num2=num1.splice(1,2);
+console.log(num2);
