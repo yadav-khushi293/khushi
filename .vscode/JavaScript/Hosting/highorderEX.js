173f44bfb85e2b5649832880f5acf60432e70267
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-08-15 21:57:20 +0530

    xyz

---
+++ .vscode/JavaScript/Hosting/highorderEX.js
@@ -0,0 +1,38 @@
+// //map
+// const number=[,1,2,3,4,5,6,7,8];
+// const mynumber=number.filter((num)=>{
+//      return num>5;
+// });
+// console.log(mynumber);
+
+
+
+// const number=[,1,2,3,4,5,6,7,8];
+// const mynumber=number.map((num)=>{
+//      return num/20;
+// });
+// console.log(mynumber);
+
+// const number=[,1,2,3,4,5,6,7,8];
+// let number2=number.reduce((acc,curr)=>acc+curr,0);
+// console.log(number2)
+
+
+// const num = [1,2,3,4,5,6,7,8];
+// const num2 = num.filter((mum)=>{
+//     return mum===5;
+// });
+// console.log(num2);
+
+
+
+//  const num = [1,2,3,4,5,6,7,8];
+// const num2 = num.map((mum)=>{
+//     // return mum===5;
+//     return mum+70;
+// });
+// console.log(num2);
+
+// const num = [1,2,3,4,5,6,7,8];
+// const num2=num.reduce((acc,curr)=>acc+curr,0);
+// console.log(num2)
