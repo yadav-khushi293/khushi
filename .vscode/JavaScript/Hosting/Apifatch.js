ab18e498c80299873c53c300f173a1b8f21e7ffe
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-08-30 14:39:35 +0530

    xyz

--- .vscode/JavaScript/Hosting/Apifatch.js
+++ .vscode/JavaScript/Hosting/Apifatch.js
@@ -12,31 +12,36 @@ const appendsFunc = (data) => {
 
   data.forEach((element) => {
     let cardDiv = document.createElement('div');
-     let title = document.createElement('h4');
-     let price =document.createElement('h3');
-     let description =document.createElement('h6');
-     let category = document.createElement('h3');
-      let img = document.createElement('img');
-      let rating =document.createElement('div');
-      let rate = document.createElement('h3')
-        let count =document.createElement('h3');
+    let title = document.createElement('h4');
+    let price = document.createElement('h3');
+    let description = document.createElement('h6');
+    let category = document.createElement('h3');
+    let img = document.createElement('img');
+    let rating = document.createElement('div');
+    let rate = document.createElement('h3');
+    let count = document.createElement('h3');
 
-
-    cardDiv.className ='card_div';
-     description.className="text_div";
-     title.className="title_div";
-     rating.className="reating_1"
+    cardDiv.className = 'card_div';
+    description.className = "text_div";
+    title.className = "title_div";
+    rating.className = "reating_1";
 
     img.src = element.image;
     title.innerText = element.title;
-    price.innerText= ` price : ${element.price}`;
-    category.innerText=`category : ${element.category}`;
-    description.innerText=element.description;
-    rate.innerText=`Rate : ${element.rating.rate}`;
-    count.innerText=`count : ${element.rating.count}`;
+    price.innerText = `Price : ${element.price}`;
+    category.innerText = `Category : ${element.category}`;
+    description.innerText = element.description;
+    rate.innerText = `Rate : ${element.rating.rate}`;
+    count.innerText = `Count : ${element.rating.count}`;
 
-    rating.append(rate,count);
-    cardDiv.append(img,title,price,category,description,rating);
+    rating.append(rate, count);
+    cardDiv.append(img, title, price, category, description, rating);
+
+    // 🔥 Add click event
+    cardDiv.addEventListener("click", () => {
+      localStorage.setItem("product", JSON.stringify(element));
+      window.location.href = "produc.html";
+    });
 
     dataShow.append(cardDiv);
   });
