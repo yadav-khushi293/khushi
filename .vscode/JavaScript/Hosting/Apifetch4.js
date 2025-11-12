95b962ebfeb9ffb427347fd31348697273139a76
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-11-12 14:07:53 +0530

    code

---
+++ .vscode/JavaScript/Hosting/Apifetch4.js
@@ -0,0 +1,17 @@
+
+for(var i=0; i<3; i++){
+    setTimeout(()=>console.log(i),100)
+}
+
+const arra =[1,2,3,4,5,6]
+console.log(arra)
+
+arra.pop(6)
+
+const object ={
+    "name":"khushi",
+    "year":2004,
+    "month":"jan",
+}
+
+console.log(object.year)
