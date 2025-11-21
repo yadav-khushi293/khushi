d0162a703cbd919ab9420b0e375990e8a869a1b0
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-11-21 16:42:51 +0530

    api

--- dummy.js
+++
@@ -1,8 +0,0 @@
- function myFunction(){
-        let useName=document.getElementById("user-name").value;
-        console.log(useName);       
-    }
-
-console.log("khushi");
-console.log("khushi");
-console.log("khusji")
