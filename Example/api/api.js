d93e9e6afea563b7310a2a7e6e42edd0321b8bb0
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-11-29 21:56:22 +0530

    db json

--- Example/api/api.js
+++ Example/api/api.js
@@ -11,23 +11,25 @@ const appendsFunc = (data) => {
   let dataShow = document.getElementById('info');
 
   data.forEach((videoLink) => {
-  let video_1=document.createElement('div');
-  let cardDiv = document.createElement('div');
-  let video = document.createElement('video');
+    let video_1 = document.createElement('div');
+    let cardDiv = document.createElement('div');
+    let video = document.createElement('video');
+    
+    cardDiv.className = "cardDiv";
+    video_1.className = "video_1";
 
-  cardDiv.className ="cardDiv";
-   video_1.className="video_1";
-  video.src = videoLink; 
-  // video.controls = true;
-  video.autoplay = true;
-  video.loop = true;
-  video.width = 400;
-   video_1.append(video)
-  cardDiv.append(video_1);
-  dataShow.append(cardDiv);
-});
-
+    video.src = videoLink;
+    video.autoplay = true;
+    video.loop = true;
+    video.muted = true; // ye Important hai for autoplay
+    video.width = 300;
+    video_1.append(video);
+    cardDiv.append(video_1);
+    dataShow.append(cardDiv);
+  });
 };
+
+
 
 
 
