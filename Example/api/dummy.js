42978c6605350fbcf9a365373279a0fa31acd5f4
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-12-08 17:40:18 +0530

    code

--- Example/api/dummy.js
+++ Example/api/dummy.js
@@ -1,48 +1,49 @@
 const api = `https://khushi-uedn.onrender.com/Luxury_perfumes`;
 
 const Apicall = () => {
-    fetch(api)
-        .then((res) => res.json())
-        .then((res) => database(res))
-        .catch((err) => console.log(err));
+  fetch(api)
+    .then((res) => res.json())
+    .then((res) => database(res))
+    .catch((err) => console.log(err));
 };
-
 
 const database = (data) => {
-    const maindiv = document.getElementById("info");
+  const maindiv = document.getElementById("info");
 
-    data.forEach((el) => {
+  data.forEach((el) => {
+    const card = document.createElement("div");
+    const title = document.createElement("p");
+    const price = document.createElement("p");
+    const imgContainer = document.createElement("div");
+    const slider = document.createElement("div");
+    const figure = document.createElement("figure");
 
-        const card = document.createElement("div");
-        const title = document.createElement("p");
-        const price = document.createElement("p");
-        const imgContainer = document.createElement("div");
-        const slider = document.createElement("div");
-        const figure = document.createElement("figure");  // correct tag
+    title.className = "title_head";
+    price.className = "price";
+    imgContainer.className = "imgContainer";
+    slider.className = "slider";
 
-        
-        
-        title.className = "title_head";
-        price.className = "price";
-        imgContainer.className = "imgContainer";
-        slider.className = "slider";
+    title.innerHTML = el.title;
+    price.innerHTML = el.parice;
 
-        title.innerHTML = el.title;
-        price.innerHTML = el.price || el.parice; // support both spellings
-
-        // ---- ADD IMAGES ----
-        el.img.forEach((url) => {
-            const img = document.createElement("img");
-            img.src = url;
-            figure.appendChild(img);
-        });
-
-        slider.appendChild(figure);
-        imgContainer.appendChild(slider);
-
-        card.append(imgContainer, title, price);
-        maindiv.append(card);
+    // Add images
+    el.img.forEach((url) => {
+      const img = document.createElement("img");
+      img.src = url;
+      figure.appendChild(img);
     });
-};
 
-Apicall();
+    slider.appendChild(figure);
+    imgContainer.appendChild(slider);
+    card.append(imgContainer, title, price);
+    maindiv.append(card);
+
+    slider.addEventListener("mouseenter", () => {
+      figure.style.animation = "slider 6s ease-in-out forwards";
+    });
+
+    slider.addEventListener("mouseleave", () => {
+      figure.style.animation = "slider 6s ease-in-out reverse forwards";
+    });
+  });
+};
